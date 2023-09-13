7c649d46de9a1192d5ae9a9ee8da92495a35931c
Megakuul <102254494+Megakuul@users.noreply.github.com>
2023-09-13 22:00:32 +0200

    Update go-mode.el

--- go-mode.el
+++ go-mode.el
@@ -10,3 +10,20 @@
   :ensure t
   :commands (lsp lsp-deferred)
   :hook (go-mode . lsp-deferred))
+
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode))
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode
+  :config
+  (setq lsp-ui-doc-enable t
+        lsp-ui-doc-use-childframe t
+        lsp-ui-doc-position 'top
+        lsp-ui-doc-include-signature t
+        lsp-ui-sideline-enable t
+        lsp-ui-flycheck-enable t
+        lsp-ui-sideline-ignore-duplicate t))
