947bf2a26f5b325588bbf226b7bd6c07ab0d799f
Megakuul <megakuul@gmx.ch>
2023-12-03 12:55:27 +0100

    Updated emacs conf

--- cpp-mode.el
+++ cpp-mode.el
@@ -1,31 +1,8 @@
 ;; Add this to your emacs-conf if you use cpp
 ;; requires ccls to be setup on the system
 
-(require 'lsp-mode)
-(require 'company)
 (require 'ccls)
 
 (use-package ccls
   :hook ((c-mode c++-mode objc-mode) .
          (lambda () (require 'ccls) (lsp))))
-
-(use-package company
-  :ensure t
-  :config
-  (global-company-mode))
-
-(use-package lsp-ui
-  :ensure t
-  :commands lsp-ui-mode
-  :config
-  (setq lsp-ui-doc-enable t
-        lsp-ui-doc-use-childframe t
-        lsp-ui-doc-position 'top
-        lsp-ui-doc-include-signature t
-        lsp-ui-sideline-enable t
-        lsp-ui-flycheck-enable t
-        lsp-ui-sideline-ignore-duplicate t))
-
-(define-key lsp-mode-map (kbd "C-c d") 'lsp-ui-doc-show)
-(define-key lsp-mode-map (kbd "C-c f") 'lsp-describe-thing-at-point)
-(define-key lsp-mode-map (kbd "C-f") 'company-complete)
--- go-mode.el
+++ go-mode.el
@@ -1,9 +1,7 @@
 ;; Add this to your emacs-conf if you use go
 ;; requires gopls to be setup on the system
 
-(require 'lsp-mode)
 (require 'go-mode)
-(require 'company)
 
 (setenv "PATH" (concat "~/go/bin:" (getenv "PATH")))
 (add-to-list 'exec-path "~/go/bin")
@@ -15,25 +13,3 @@
   :ensure t
   :commands (lsp lsp-deferred)
   :hook (go-mode . lsp-deferred))
-
-(use-package company
-  :ensure t
-  :config
-  (global-company-mode))
-
-(use-package lsp-ui
-  :ensure t
-  :commands lsp-ui-mode
-  :config
-  (setq lsp-ui-doc-enable t
-        lsp-ui-doc-use-childframe t
-        lsp-ui-doc-position 'top
-        lsp-ui-doc-include-signature t
-        lsp-ui-sideline-enable t
-        lsp-ui-flycheck-enable t
-        lsp-ui-sideline-ignore-duplicate t))
-
-
-(define-key lsp-mode-map (kbd "C-c d") 'lsp-ui-doc-show)
-(define-key lsp-mode-map (kbd "C-c f") 'lsp-describe-thing-at-point)
-(define-key lsp-mode-map (kbd "C-f") 'company-complete)
